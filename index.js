bc130790fa68dc794cd963b62c3d20d4ab13d4ab
MIUI45 <70149057+MIUI45@users.noreply.github.com>
2020-09-13 20:51:36 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const discord =require("discord.js")//Conectando ao discord.js
+const Database =require("MIUIDB")//Conectando a Database 
+
+// Agora o bot está pronto nem precisa olhar é só chegar copiar e colar
+
+
+console.log("Bobinho acha que eu ia perder meu tempo codando para você ? Estuda que tu tem futuro :3") 
+
+
+// Fim da index 
